22b770afb3bdee0f8bfb08f0febafd6e1e395ade
jian9.huang@meicloud.com <106025154@qq.com>
2020-05-21 18:05:10 +0800

    preview html complete

--- routes/pageHandler.js
+++ routes/pageHandler.js
@@ -8,37 +8,11 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
-/* GET users listing. */
-/*router.get('/:id', function (req, res, next) {
-    console.log(url.format({
-        pathname: path.join(__dirname, `html/${req.params.id}.html`),
-        protocol: 'file',
-        slashes: true
-    }));
-    console.log(url.format({
-        pathname: path.join(__dirname, `html/${req.params.id}.html`),
-        slashes: true
-    }));
-    console.log(url.format({
-        pathname: path.join(__dirname, `../html/${req.params.id}.html`),
-    }));
-    res.render('index', {title: req.params.id}, (err, html) => {
-
-        fs.writeFile(url.format({
-            pathname: path.join(__dirname, `../html/${req.params.id}.html`),
-            // pathname: path.join(__dirname, `html/${req.params.id}.html`),
-        }), html, err => console.log(err));
-
-        res.send(html);
-    });
-});*/
-
 const postHandler = (req, res, next) => {
-    const {htmlStr, name} = req.body;
+    const {htmlStr, name, token} = req.body;
     console.log(req.body);
     if (htmlStr && name) {
-        res.render('index', {htmlStr, name}, (err, html) => {
-
+        res.render('index', {htmlStr, name, token}, (err, html) => {
             fs.writeFile(url.format({
                 pathname: path.join(__dirname, `../html/${name}_${(new Date).getTime().toString(16)}.html`),
             }), html, err => {
