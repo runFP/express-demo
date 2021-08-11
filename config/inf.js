ef123212007630c6e6c02212ef039bb37c41df8f
HJ <34636676+runFP@users.noreply.github.com>
2021-08-11 10:53:33 +0800

    Update inf.js

--- config/inf.js
+++ config/inf.js
@@ -1,5 +1,5 @@
 const connectionString = 'mongodb+srv://charts:charts666@cluster0-paw64.mongodb.net/test?retryWrites=true&w=majority';
-const DB_URL = 'mongodb://runfp:rfp123456@120.27.68.119:27017';
+const DB_URL = 'mongodb://runfp:rfp123456@xxx.xxx.xxx.xxx:xxxx';
 //runfp:rfp123456
 
 module.exports.connectionString = connectionString;
